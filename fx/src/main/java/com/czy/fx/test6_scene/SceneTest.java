4edc8e49dbd8bec2cda7a1492af70e36c90999bf
dimandsun <94chenzy>
2020-04-29 01:38:21 +0800

    javafx

--- fx/src/main/java/com/czy/fx/test6_scene/SceneTest.java
+++ fx/src/main/java/com/czy/fx/test6_scene/SceneTest.java
@@ -4,6 +4,7 @@ package com.czy.fx.test6_scene
 import javafx.application.Application;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
