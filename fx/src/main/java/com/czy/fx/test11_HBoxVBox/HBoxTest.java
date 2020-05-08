c4cc8c4f8e7707a1c91222be2dbf74e5f8afaeef
dimandsun <94chenzy>
2020-05-08 22:17:57 +0800

    提交intellij自动生成的配置文件

---
+++ fx/src/main/java/com/czy/fx/test11_HBoxVBox/HBoxTest.java
@@ -0,0 +1,16 @@
+package com.czy.fx.test11_HBoxVBox;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author chenzy
+ * @description
+ * @since 2020/5/3
+ */
+public class HBoxTest extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
