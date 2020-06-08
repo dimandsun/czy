4ab175fb929c97a04b99db7729aa0cc6078b6aa5
dimandsun <dimandsun@163.com>
2020-06-08 16:22:44 +0800

    javase

---
+++ javase-study/src/main/java/com/czy/javase/study/test0_helloWorld/HelloWord.java
@@ -0,0 +1,11 @@
+package com.czy.javase.study.test0_helloWorld;
+
+/**
+ * @author chenzy
+ * @since 2020-06-08
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello world!\n你好世界！");
+    }
+}
