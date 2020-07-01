39d8b88d9810dc2498b15a3e0e204b97689cd9a9
dimandsun <dimandsun@163.com>
2020-07-01 19:01:37 +0800

    新增模块：httpcontainer

--- httpcontainer/src/test/java/Client.java
+++ httpcontainer/src/test/java/Client.java
@@ -1,34 +1,35 @@
-package socket.http;
 
+import com.czy.util.io.IOUtil;
 import com.czy.util.tcp.HttpRequestUtil;
-
+import com.czy.util.tcp.SocketUtil;
 /**
  * @author chenzy
- * @since 2020-06-18
+ * @date 2020-06-29
  */
-public class ClientHTTP {
+public class Client {
+
     public static void main(String[] args) throws InterruptedException {
+        var host = "127.0.0.1";
+        var port = 10089;
+
         int i=1;
         while (true){
-
-            String a=HttpRequestUtil.sendGet("http://127.0.0.1:8080/abc",null);
-            System.out.println(a);
-            /*//获取连接
-            var socket = getConnection("127.0.0.1", 8080);
+            //获取连接
+            var socket = SocketUtil.getConnection(host, port);
             if (socket==null){
                 System.out.println("无法连接指定服务器，请检查");
                 continue;
             }
+            HttpRequestUtil
             //发送数据
-            sendData(socket, "数据" + i);
+            SocketUtil.sendData(socket, "数据" + i);
             //读取数据
-            String result = readData(socket);
+            String result = SocketUtil.readData(socket);
             //关闭连接
-            close(socket);
+            IOUtil.close(socket);
             System.out.println(result);
-            Thread.sleep(1000);*/
+            Thread.sleep(1000);
             i++;
         }
-
     }
 }
--- util/src/main/java/com/czy/util/tcp/HttpRequestUtil.java
+++ util/src/main/java/com/czy/util/tcp/HttpRequestUtil.java
@@ -1,5 +1,7 @@
 package com.czy.util.tcp;
 
+import com.czy.util.text.StringUtil;
+
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
@@ -15,11 +17,22 @@ public class HttpRequestUtil {
 
 
 
-    private static URLConnection getConnection(URL url) throws IOException {
-        URLConnection connection = url.openConnection();
-        connection.setConnectTimeout(30 * 1000);
-        connection.setReadTimeout(30 * 1000);
-        return connection;
+    private static URLConnection getConnection(URL url){
+        try {
+            var connection = url.openConnection();
+            connection.setConnectTimeout(30 * 1000);
+            connection.setReadTimeout(30 * 1000);
+            connection.setRequestProperty("Charsert", "UTF-8"); //设置请求编码
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            connection.connect();
+            return connection;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /**
@@ -36,26 +49,9 @@ public class HttpRequestUtil {
             param="";
         }
         try {
-            String urlNameString = url + "?" + param;
-            if (param == null || param.equals(""))
-                urlNameString = url;
+            String urlNameString = StringUtil.isBlank(param)?url:url+"?"+param;
             URL realUrl = new URL(urlNameString);
-            // 打开和URL之间的连接
             URLConnection connection = getConnection(realUrl);
-            // 设置通用的请求属性
-            connection.setRequestProperty("Charsert", "UTF-8"); //设置请求编码
-            connection.setRequestProperty("accept", "*/*");
-            connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 建立实际的连接
-            connection.connect();
-            // 获取所有响应头字段
-            Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : map.keySet()) {
-                // System.out.println(key + "--->" + map.get(key));
-            }
             // 定义 BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(
                     connection.getInputStream(), "UTF-8"));
@@ -63,10 +59,12 @@ public class HttpRequestUtil {
             while ((line = in.readLine()) != null) {
                 result += new String(line.getBytes());
             }
-
         } catch (Exception e) {
             return null;
         }
+
+
+
         // 使用finally块来关闭输入流
         finally {
             try {
