efe2c4c27fda2428ca79daca8ba1a7ecea62fab1
dimandsun <dimandsun@163.com>
2020-08-26 18:50:43 +0800

    html数据双向绑定

--- question_bank/src/main/resource/static/js/bind/Binder.js
+++ question_bank/src/main/resource/static/js/bind/Binder.js
@@ -13,7 +13,7 @@ import util from '../util.js';
  </div>
 <script type="module">
   import {Binder} from './Binder.js';
-  let binder = new Binder({
+  const binder = new Binder({
     el: '#app',
     data: {
       count: 0,
@@ -36,7 +36,14 @@ export class Binder {
     methods,
     data
   }) {
-    this.$data = data;
+    this.bindReg=/{{(?<bindText>[(a-zA-Z_\.+=\*\/)].*)}}/g;//绑定表达式
+    this.$data={
+      data:data,
+      eval: (code, param) => {
+        let realCode=util.repalce(code,/([(a-zA-Z_)].*)/g,'param.');
+        return eval(realCode);
+      }
+    }
     this.$methods = methods;
     /* 初始化绑定对象*/
     this._binding = {};
@@ -46,7 +53,7 @@ export class Binder {
     const handler = this._getHandler(data);
     /* 得到代理对象*/
     return new Proxy({ ...this.$el,
-      ...this.$data,
+      ...this.$data.data,
       ...this.$methods
     }, handler);
   }
@@ -75,7 +82,7 @@ export class Binder {
         if (!(bindData in me._binding)) {
           throw new ReferenceError(`绑定器没有为${bindData}的属性或方法`);
         }
-        for (let watcher of [...me._binding[bindData]]) {
+        for (const watcher of [...me._binding[bindData]]) {
           watcher.update(value);
         }
         /* 返回*/
@@ -89,76 +96,169 @@ export class Binder {
   */
   _compileParent(root) {
     if (root.hasChildNodes() && root.children.length > 0) {
-      for (let node of [...root.children]) {
+      for (const node of [...root.children]) {
         this._compileChild(node, true);
       }
     }
     this._compileChild(root, false);
   }
-  _compileChild(node,isRecursion) {
-    if (isRecursion&&node.hasChildNodes() && node.children.length > 0) {
+  _compileChild(node, isRecursion) {
+    if (isRecursion && node.hasChildNodes() && node.children.length > 0) {
       this._compileParent(node);
     }
-    // <option b-for="qType in qTypeLisst">${qType}</option>
-    
-    
-    /* 初始化元素属性(包括监听)*/
-    for (let entry of [...node.attributes]) {
+    // <option b-for="qType of qTypeLisst">${qType}</option>
+    /* 初始化元素属性(包括监听),以:开头的属性*/
+    this._compileNodeAttr(node);
+
+    /* 初始化带有b-for属性的元素，没有b-for属性才_compileNodeText*/
+    if (!this._compileNodeForeach(node)) {
+      /* 初始化元素的文本内容：*/
+      this._compileNodeText(node);
+    }
+  }
+  /**
+   * 初始化遍历元素。没有b-for属性(或者没有正确编译b-for)返回false，有返回true
+   * @param {Object} node
+   */
+  _compileNodeForeach(node) {
+    if (!node.hasAttribute("b-for")) {
+      return false;
+    }
+    /* 得到b-for表达式*/
+    const forGroups = util.matchingFirstStr(/(?<item>[(a-zA-Z_)]*) of (?<list>[(a-zA-Z_)]*)/g, node.getAttribute(
+      "b-for"));
+    const itemName = forGroups.item;
+    const listName = forGroups.list;
+    if (util.isBlank(itemName) || util.isBlank(listName)) {
+      return false; /* b-for表达式错误 */
+    }
+    const list = this.$data.data[listName];
+    if (!util.isIterable(list)) {
+      return false; //不可遍历
+    }
+    node.removeAttribute("b-for");
+    /* 元素的子元素，暂时不考虑此情况begin*/
+    //@TODO
+    /* 元素的子元素，暂时不考虑此情况end*/
+    /*判断元素文本内容是否有需要绑定的数据：*/
+    let notNeedBind = true;
+    for (const cn of [...node.childNodes]) {
+      if (cn.nodeType === 3 && cn.nodeValue.indexOf('{{') !== -1) {
+        if (util.matchingFirstStr(this.bindReg, cn.nodeValue).bindText) {
+          notNeedBind = false;
+          break;
+        }
+      }
+    }
+    if (notNeedBind) {
+      return true;
+    }
+    /* 给元素的文本内容赋初始值*/
+    const id = node.getAttribute("id");
+    const name = node.getAttribute("name");
+    list.forEach((item, index, list) => {
+      let itemNode = null;
+      itemNode = node.cloneNode(true);
+      node.parentNode.appendChild(itemNode);
+      /* id、name加上index，防止id、name重复*/
+      if (id) {
+        itemNode.setAttribute("id", "".concat(id, index)); //"".concat防止id为数字时，拼接变成加法运算
+      }
+      if (name) {
+        itemNode.setAttribute("name", "".concat(name, index)); //"".concat防止name为数字时，拼接变成加法运算
+      }
+      for (const cn of [...itemNode.childNodes]) {
+        if (cn.nodeType === 3 && cn.nodeValue.indexOf('{{') !== -1) {
+          const bindText = util.matchingFirstStr(this.bindReg, cn.nodeValue).bindText;
+          if (bindText && (bindText.indexOf(itemName) !== -1 || bindText.indexOf(listName) !== -1 || bindText.indexOf(
+              "index") !== -1)) {
+            let code = bindText.replace(new RegExp(itemName, "g"), "item")
+              .replace(new RegExp(listName, "g"), "list");
+            cn.nodeValue = this.$data.eval(code, { ...this.$data.data,
+              "item": item,
+              "index": index,
+              "list": list
+            });
+            this._pushWatcher(list, new Watcher(cn, "v-for", code));
+          }
+        }
+      }
+    });
+    /* 功成身退，移除元素*/
+    node.remove();
+    return true;
+  }
+  /**
+   * 初始化元素属性(包括监听)
+   * @param {Object} node
+   */
+  _compileNodeAttr(node) {
+    for (const entry of [...node.attributes]) {
       if (entry.name.indexOf(':') !== 0) {
         continue;
       }
       /* 真正的属性名*/
-      let realAttr = entry.name.substr(1);
+      const realAttr = entry.name.substr(1);
       /* 需要绑定的数据(包括方法)名 */
-      let bindData = node.getAttribute(entry.name);
-      if (bindData in this.$data) {
+      const bindData = node.getAttribute(entry.name);
+      if (bindData in this.$data.data) {
         /* 绑定的是属性*/
         /* 初始化属性*/
-        node.setAttribute(realAttr, this.$data[bindData]);
+        node.setAttribute(realAttr, this.$data.data[bindData]);
         /* 注册观察者*/
         this._pushWatcher(bindData, new Watcher(node, realAttr));
       } else if (bindData in this.$methods) {
         /* 绑定的是方法*/
-        /* 初始化监听方法，如果无效，改成node.addEventListener(realAttr, this.$methods[bindData]);*/
-        // node.setAttribute(realAttr,this.$methods[bindData]);
+        /* 初始化监听方法*/
         node.addEventListener(realAttr, this.$methods[bindData]);
       } else {
         /* 抛出异常*/
         throw new ReferenceError(`绑定器没有为${bindData}的数据或方法`);
       }
     }
-    /* 初始化元素的文本内容：innerHTML，innerText都不能满足*/
-    for(let cn of[...node.childNodes]){
-      if(cn.nodeType===3){//文本元素
-        if (cn.nodeValue.indexOf('${') !== -1) {
-          let bindData = util.matchingFirstStr(/\${(?<bindData>[(a-zA-Z_)]*)}/g, cn.nodeValue).bindData;
-          if (bindData && (bindData in this.$data)) {
-            /* 绑定的是文本*/
+  }
+  /**
+   * 初始化元素的文本内容：innerHTML，innerText都不能满足
+   * @param {Object} node
+   */
+  _compileNodeText(node) {
+    for (const cn of [...node.childNodes]) {
+      if (cn.nodeType === 3) { //文本元素
+        if (cn.nodeValue.indexOf('{{') !== -1) {
+          const bindText = util.matchingFirstStr(this.bindReg, cn.nodeValue).bindText;
+
+          if (bindText) {
             /* 初始化文本*/
-            cn.nodeValue = this.$data[bindData];
+            cn.nodeValue = this.$data.eval(bindText, this.$data.data);
+            // cn.nodeValue = this.$data.data[bindText];
             /* 注册观察者*/
-            this._pushWatcher(bindData, new Watcher(cn, 'innerHTML'));
+            this._pushWatcher(bindText, new Watcher(cn, 'nodeValue'));
           }
         }
       }
     }
   }
-  _compileChild
 }
 /*
  watcher的作用是 链接Observer 和 Compile的桥梁，能够订阅并收到每个属性变动的通知，
  执行指令绑定的响应的回调函数，从而更新视图。
 */
 class Watcher {
-  constructor(node, attr) {
+  constructor(node, attr, code) {
     this.node = node;
     this.attr = attr;
+    this.code = code;
   }
   update(value) {
-    if(this.node.nodeType==3){
-     this.node.nodeValue=value;
-    }else{
-    this.node[this.attr] = value;
+    if (this.node.nodeType == 3) {
+      this.node.nodeValue = value;
+    } else if (this.attr === "v-for") {
+      const list = value;
+      list.forEach((item, index, list) => {
+        this.node.nodeValue = eval(this.code);
+      })
+    } else {
+      this.node[this.attr] = value;
     }
   }
 }
